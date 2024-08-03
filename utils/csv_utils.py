1cbb2184891cc808eb0b47f0b738736bb87c8097
MasonDG21 <mason.galyon@gmail.com>
2024-08-03 15:49:10 -0500

    handles the new data structure

---
+++ utils/csv_utils.py
@@ -0,0 +1,24 @@
+import csv
+
+class CSVUtils:
+    @staticmethod
+    def write_to_csv(data, filename):
+        if not data:
+            return
+
+        fieldnames = ["name", "title", "email", "linkedin"]
+        
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+
+    @staticmethod
+    def read_from_csv(filename):
+        data = []
+        with open(filename, 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data.append(row)
+        return data
